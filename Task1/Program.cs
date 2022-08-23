761a73aadaf6a97445d90d2302b76518ef98a42e
ФЕСС - <mr.fesenko@inbox.ru>
2022-08-23 16:25:46 +0300

    added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int [] numbers = new int[4];
+int i, count;
+i = 0;
+count = 0;
+for (i = 0; i < numbers.Length;)
+{        
+    numbers [i] = new Random().Next(99, 1000);        
+    Console.Write($"{numbers [i]} ");
+        if ((numbers [i]% 2)<1)
+        {
+            count+=1;
+        }  
+ 
+    i++;
+ }
+ Console.Write ($"->{count}");  
