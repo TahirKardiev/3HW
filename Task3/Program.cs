002feda3bd59740b1a86cf467bf38e7ca1b24c92
ФЕСС - <mr.fesenko@inbox.ru>
2022-08-23 17:35:56 +0300

    added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿double [] numbers = new double[4];
+int i; double max, min;
+i = 0;
+max = 0;
+min = new Random().Next(0, 99);
+for (i = 0; i < numbers.Length;)
+{        
+    numbers [i] = new Random().Next(0, 99);
+    numbers[i]=numbers[i]/new Random().Next(0, 99);        
+    Console.Write($"{numbers [i]} ");
+        if (numbers [i]>max)
+        {
+            max=numbers [i];
+        }
+        if (numbers [i]<min)
+        {
+            min=numbers [i];
+        } 
+ 
+    i++;
+ }
+ Console.Write($"->{max-min}");
